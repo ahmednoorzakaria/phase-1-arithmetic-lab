a62c23cf8da60068aee31016bea0aa9db865de29
ahmednoorzakaria <ahmednoor.zakaria@student.moringaschool.com>
2023-06-12 16:13:58 +0300

    completeted Arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+function randomInteger() {
+    return Math.floor(Math.random() * 10) + 1;
+  }
+  
+  let random = randomInteger();  
+
+let num3 = 19;
+let num4 = 5;
+let mod = num3%num4; 
+
+let numberset = [ 4,4,6,20];
+let max = Math.max(...numberset);
